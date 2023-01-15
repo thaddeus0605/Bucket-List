a189323c9068a2b3fe5decf3cebb798806dab8a4
thaddeus0605 <tdronski@gmail.com>
2023-01-15 16:02:20 -0600

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Thaddeus Dronski on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bucket List/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bucket List
+//
+//  Created by Thaddeus Dronski on 1/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
